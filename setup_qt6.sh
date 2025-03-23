1ca9a3773c0609416a9f57875334078028a03674
emilie <egunti@asu.edu>
2025-03-23 07:31:26 +0000

    Added Qt6 setup script for ZIP download

---
+++ setup_qt6.sh
@@ -0,0 +1,8 @@
+echo "Downloading Qt6..."
+wget -O Qt6.zip "https://drive.google.com/file/d/1VYo06ttQjT-eF-z9czQE_lJ8-aLyTIth/view?usp=sharing"
+
+echo "Extracting Qt6" 
+unzip Qt6.zip
+rm Qt6.zip
+echo "Qt6 setup complete"
+
