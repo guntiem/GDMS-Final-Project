aebf1174420298fc29941b44faa11dd05114caf6
emilie <egunti@asu.edu>
2025-03-26 21:30:56 +0000

    Removed cache files and updated .gitignore

--- sampleApp/build/Desktop_Qt_6_8_2-Debug/.qt/appsampleApp_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appsampleApp")
-set(working_dir "/home/egunti/GDMS-Final-Project/sampleApp")
-set(src_and_dest_list
-    "/home/egunti/GDMS-Final-Project/sampleApp/images.qrc"
-    "/home/egunti/GDMS-Final-Project/sampleApp/build/Desktop_Qt_6_8_2-Debug/sampleApp/images.qrc"
-
-)
-set(timestamp_file "/home/egunti/GDMS-Final-Project/sampleApp/build/Desktop_Qt_6_8_2-Debug/.qt/appsampleApp_res.txt")
