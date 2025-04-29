db86df0843ce0880b10b67239f20c8678a0c6e17
Anthony Hong <148808144+AirFuelRatio@users.noreply.github.com>
2025-04-29 10:19:16 -0700

    Update and rename automate_tests.sh to automate_tests_script.sh

--- automate_tests_script.sh
+++ automate_tests_script.sh
@@ -3,7 +3,7 @@
 test_failed=0
 
 # Run test suite 1
-'/home/tony/squish-for-qt-8.0.0/bin/squishrunner' --testsuite '/home/tony/Desktop/final_application/GDMS-Final-Project/testSuites/suite_lockscreenTesting' --local > lockscreentestresults.txt 2>&1
+'/path/to/squish-for-qt-8.0.0/bin/squishrunner' --testsuite '/path/to/suite_lockscreenTesting' --local > lockscreentestresults.txt 2>&1
 if [ $? -ne 0 ]; then
     echo "Test suite 1 failed."
     test_failed=1
