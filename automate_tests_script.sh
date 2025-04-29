d561fd3a4fdfce2a3f00870be91c10bdaa17619e
egittens <egittens5250@gmail.com>
2025-04-29 12:31:52 -0700

    added loop to automated script and updated ritviks test suites

--- automate_tests_script.sh
+++ automate_tests_script.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-test_failed=0
+pass_or_fail=0
+squishrunner_path="/home/vboxuser/squish-for-qt-8.1.0/bin/squishrunner"
+testcenter_path="/home/vboxuser/testcenter-4.1.1-linux-x64/"
 
-# Run test suite 1
-'/path/to/squish-for-qt-8.0.0/bin/squishrunner' --testsuite '/path/to/suite_lockscreenTesting' --local > lockscreentestresults.txt 2>&1
-if [ $? -ne 0 ]; then
-    echo "Test suite 1 failed."
-    test_failed=1
-else
-    echo "Test suite 1 executed successfully."
-fi
+for suite_path in testSuites/suite_*/; do
+    suite_name=$(basename "$suite_path")
+    "$squishrunner_path" --testsuite "testSuites/$suite_name" --exitCodeOnFail 1 --reportgen xml3.4,"testResults/${suite_name}Results/"
+    
+    if [ $? -eq 0 ]; then
+        echo "${suite_name} executed successfully."
+    else
+        echo "${suite_name} failed."
+        pass_or_fail=1
+    fi
 
-
-
-
-
-if [ $test_failed -ne 0 ]; then
-    echo "One or more test suites failed."
-else
+    zip -r "testResults/${suite_name}Results.zip" "testResults/${suite_name}Results/"  
+    "$testcenter_path/bin/testcentercmd" --url=http://localhost:8800 --token=49_337cZ7PCLY37SmciZAUR9-7fK4tQVFLhGP-0ssmM upload sampleApp "testResults/${suite_name}Results.zip"    
+done   
+  
+if [ $pass_or_fail -eq 0 ]; then
     echo "All test suites passed!"
+else
+    echo "One or more test suites failed."
 fi
 
-exit $test_failed
+exit $pass_or_fail
+
