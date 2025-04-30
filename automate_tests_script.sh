bb22f5a234e2afa4811570ef23ebe637c9d3c363
egittens <142512562+egittens@users.noreply.github.com>
2025-04-30 09:45:52 -0700

    Update automate_tests_script.sh to use env variables

--- automate_tests_script.sh
+++ automate_tests_script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 pass_or_fail=0
-squishrunner_path="/home/vboxuser/squish-for-qt-8.1.0/bin/squishrunner"
-testcenter_path="/home/vboxuser/testcenter-4.1.1-linux-x64/"
 
 for suite_path in testSuites/suite_*/; do
     suite_name=$(basename "$suite_path")
-    "$squishrunner_path" --testsuite "testSuites/$suite_name" --exitCodeOnFail 1 --reportgen xml3.4,"testResults/${suite_name}Results/"
+    squishrunner --testsuite "testSuites/$suite_name" --exitCodeOnFail 1 --reportgen xml3.4,"testResults/${suite_name}Results/"
     
     if [ $? -eq 0 ]; then
         echo "${suite_name} executed successfully."
@@ -16,7 +14,7 @@ for suite_path in testSuites/suite_*/; do
     fi
 
     zip -r "testResults/${suite_name}Results.zip" "testResults/${suite_name}Results/"  
-    "$testcenter_path/bin/testcentercmd" --url=http://localhost:8800 --token=49_337cZ7PCLY37SmciZAUR9-7fK4tQVFLhGP-0ssmM upload sampleApp "testResults/${suite_name}Results.zip"    
+    testcentercmd --url=http://localhost:8800 --token=49_337cZ7PCLY37SmciZAUR9-7fK4tQVFLhGP-0ssmM upload sampleApp "testResults/${suite_name}Results.zip"    
 done   
   
 if [ $pass_or_fail -eq 0 ]; then
@@ -26,4 +24,3 @@ else
 fi
 
 exit $pass_or_fail
-
